07b4b3351eb985b42feb99a66146ab2106204a4f
Irina <mihailov.irina@gmail.com>
2025-01-15 21:04:05 +0200

    -added Maven project

--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -1,15 +1,19 @@
+import org.apache.logging.log4j.Logger;
 import org.automation.framework.BrowserManager;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.apache.logging.log4j.LogManager;
 
 public class DemoTest {
     BrowserManager manager = new BrowserManager();
+    Logger log = LogManager.getRootLogger();
 
     @Test
     public void browserTest() {
         manager.openBrowser();
+        log.info("Browser opened");
         WebDriver driver = manager.getDriver();
         driver.get("https://www.youtube.com");
         String url= driver.getCurrentUrl();
